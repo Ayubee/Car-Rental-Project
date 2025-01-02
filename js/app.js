aa3a0548fe34c35434a89b59dec8870d70ac8566
Ayubxon <aubsattorov@gmail.com>
2025-01-03 00:51:17 +0500

    add new SVG icons for social media and update theme toggler for brand logos

--- js/app.js
+++ js/app.js
@@ -6,4 +6,29 @@ themeToggler.addEventListener("click", () => {
     document.body.classList.contains("dark-mode")
       ? "./src/images/sun.svg"
       : "./src/images/moon.svg";
+
+  document.getElementById("brands__nissan").src =
+    document.body.classList.contains("dark-mode")
+      ? "./src/images/nissan-white.png"
+      : "./src/images/nissan-black.png";
+
+  document.getElementById("brands__toyota").src =
+    document.body.classList.contains("dark-mode")
+      ? "./src/images/toyota-white.png"
+      : "./src/images/toyota.png";
+
+  document.getElementById("brands__dodge").src =
+    document.body.classList.contains("dark-mode")
+      ? "./src/images/dodge-white.png"
+      : "./src/images/dodge.png";
+
+  document.getElementById("brands__hyundai").src =
+    document.body.classList.contains("dark-mode")
+      ? "./src/images/hyundai-white.png"
+      : "./src/images/hyundai.png";
+
+  document.getElementById("brands__jeep").src =
+    document.body.classList.contains("dark-mode")
+      ? "./src/images/jeep-white.png"
+      : "./src/images/jeep.png";
 });
